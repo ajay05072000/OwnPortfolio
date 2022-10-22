869cbc79a675651cf13d252eac7f73414e75f90b
AJAY KUMAR JHA <77736437+ajayjhaDev@users.noreply.github.com>
2022-10-22 17:16:48 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -27,7 +27,7 @@ function About() {
             <br />I am very passionate about learning new technologies.
           </p>
           <div className="text-center mb-4">
-            <a href="https://drive.google.com/file/d/1JlouoNC6LDN4lUa259cGpd4VfIVZZD7G/view">
+            <a href="https://drive.google.com/file/d/1MbK0LZXCSqtpc8LyaXqTd2z6t39nJ2T3/view">
               <button type="button" className="btn btn-outline-primary">
                 View Resume
               </button>
