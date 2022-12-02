7c61fb847f88ffa843abbf78a3ba0ea8b34ef23d
ajayjhaDev <ajaykumarjha05072000@gmail.com>
2022-12-02 11:23:01 +0530

    modify

--- src/Components/Skill.jsx
+++ src/Components/Skill.jsx
@@ -1,18 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import skills from "../Skills";
 
 function Skill() {
-  const [skill, setSkill] = useState(skills);
-
   return (
     <main className="p-4  skill_section" id="skill">
       <h1 className="text-center display-2 mb-4">Skills</h1>
 
       <ul className="list-group text-center">
-        {skill.map((ele) => (
+        {skills.map((ele) => (
           <li className={"list-group-item"} key={ele.name}>
             <strong className={"me-5"}>{ele.name}</strong>{" "}
-            <img src={ele.img} height={"30px"} width={"30px"} />
+            <img src={ele.img} height={"30px"} width={"30px"} alt={"img"} />
           </li>
         ))}
       </ul>
